787aaa649485fca217d35c4d712369516566816a
JonRom1012 <jonathan.romuald@gmail.com>
2020-08-10 00:40:00 +0200

    Updates

---
+++ precache-manifest.0f37079aef41a0946ddc48ffcfe773a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251fc95c3fae8135b6c54c10e10239c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "abe1825ed7bfde362062",
+    "url": "/monsters-rolodex/static/css/main.e15acfde.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe1825ed7bfde362062",
+    "url": "/monsters-rolodex/static/js/main.abcd85b8.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
